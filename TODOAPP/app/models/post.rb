f6a620cabb7e4ae8c529c56c69e78199f9c325c2
nobumisu-1995 <masugata1995@mailcom>
2020-11-29 13:55:48 +0900

    create method define_deadline

--- TODOAPP/app/models/post.rb
+++ TODOAPP/app/models/post.rb
@@ -5,26 +5,27 @@ class Post < ApplicationRecord
     min = (self.deadline - today)/60
     hour = min/60
     day = hour/24
-    if day >= 0
-      if day >= 1
+    if day >= 0                       #締め切り前
+      if day >= 1                     #締め切り1日以上前
         return "#{day.to_i}日"
-      else
-        if hour >= 1
+      else                            #締め切り1日未満
+        if hour >= 1                  #締め切り1日未満且つ一時間以上
           return "#{hour.to_i}時間"
-        else
+        else                          #締め切り1日未満且つ一時間未満
           return "#{min.to_i}分"
         end
       end
-    else
-      if -day >= 1
+    else                              #締め切り超過
+      if -day >= 1                    #締め切り超過1日以上
         return "#{-day.to_i}日"
-      else
-        if -hour >= 1
+      else                            #締め切り超過1日未満
+        if -hour >= 1                 #締め切り超過1日未満且つ一時間以上
           return "#{-hour.to_i}時間"
-        else
+        else                          #締め切り超過1日未満且つ一時間未満
           return "#{-min.to_i}分"
         end
       end
     end
   end
+
 end
